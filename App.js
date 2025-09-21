af121c4e84e2ee70b92158c98d624517b7d6d838
Jaczibeth <jaczicruz@gmail.com>
2025-09-21 16:10:12 -0600

    aplicacion de estructura a app

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -17,7 +18,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Presentacion">
           <Stack.Screen name="Presentacion" component={Presentacion} options={{ headerShown: false }} />
-          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="Home" component={Home}  options={{ headerShown: false }}/>
           <Stack.Screen name="RegistrarAlumno" component={RegistrarAlumno} />
           <Stack.Screen name="ListaAlumnos" component={ListaAlumnos} />
           <Stack.Screen name="EliminarAlumno" component={EliminarAlumno} />
