529c06dafa8d690a7d0d093ea5b9456a02af2b3f
Jaczibeth <jaczicruz@gmail.com>
2025-09-20 20:42:02 -0600

    Agregando una imagen con estilos

--- src/Pages/Presentacion.tsx
+++ src/Pages/Presentacion.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Image,TouchableOpacity } from 'react-native';
 import { Text, Button } from 'react-native-paper';
 export default function Presentacion() {
   return (
     <View style={styles.container}>
+          <Image source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyDeyazS5stoekyhlBR5S4UAuZ4qTAdT0NogvGvI4ZkEpxZL5fNtKIG3JN0BD6wzprKZs&usqp=CAU" }} style={styles.logo}/> 
       <Text style={styles.titulo}>Bienvenido a la App de Alumnos</Text>
       <Text style={styles.subtitulo}> Aquí podrás registrar, listar y eliminar alumnos fácilmente</Text>
     </View>
@@ -12,7 +13,7 @@ export default function Presentacion() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#def5fbff",
+    backgroundColor: "#ffffffff",
     justifyContent: "center",
     alignItems: "center",
     padding: 10,
@@ -30,5 +31,11 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginBottom: 30,
   },
+  logo: {
+    width: 220,
+    height: 220,
+    marginBottom: 20,
+  },
+
 
 });
