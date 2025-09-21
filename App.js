6fb78c796b6983b2ded4eecf82fa683d3defff89
Jaczibeth <jaczicruz@gmail.com>
2025-09-21 13:53:39 -0600

    Restructurando el app.js

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AlumnoProvider } from './src/Context/AlumnoContext';
+
 import Presentacion from './src/Pages/Presentacion';
+import Home from './src/Pages/Home';
+import RegistrarAlumno from './src/Pages/RegistrarAlumno';
+import ListaAlumnos from './src/Pages/ListaAlumnos';
+import EliminarAlumno from './src/Pages/EliminarAlumno';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Presentacion/>
-      <StatusBar style="auto" />
-    </View>
+    <AlumnoProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Presentacion">
+          <Stack.Screen name="Presentacion" component={Presentacion} options={{ headerShown: false }} />
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="RegistrarAlumno" component={RegistrarAlumno} />
+          <Stack.Screen name="ListaAlumnos" component={ListaAlumnos} />
+          <Stack.Screen name="EliminarAlumno" component={EliminarAlumno} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AlumnoProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#a5bef0ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
